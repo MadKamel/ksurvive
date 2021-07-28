0012bd1a87c899da0f7b661ead5f648be1582f22
Eric Rustrum <rustrumek@gmail.com>
2021-07-27 23:05:33 -0400

    Added empty letter sprite.
    Last commit of the day. Good night.

--- mods/letters/letters.lua
+++ mods/letters/letters.lua
@@ -8,7 +8,7 @@ minetest.register_craftitem("letters:letter", {
 
 minetest.register_craftitem("letters:letter_empty", {
 	description = "Empty Letter",
-	inventory_image = "letter.png",
+	inventory_image = "letter_empty.png",
 	stack_max = 1,
 	groups = {flammable = 1, book = 1},
 	on_use = book_on_use,
