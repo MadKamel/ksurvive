4a647ec5f1bfcb5f1f123ed08c589e28648edbc0
Eric Rustrum <rustrumek@gmail.com>
2021-08-16 16:59:58 -0700

    Removed water pollution from the game.

--- mods/pitch/init.lua
+++ mods/pitch/init.lua
@@ -1,5 +1,5 @@
 local default_path = minetest.get_modpath("pitch")
 
 dofile(default_path.."/pitch.lua")
-dofile(default_path.."/pollution.lua")
+--dofile(default_path.."/pollution.lua")
 dofile(default_path.."/plant_pollution.lua")
