1c7c39ca7dfde99032ea33aebb739599d461c7a4
Eric Rustrum <rustrumek@gmail.com>
2021-08-17 14:57:01 -0700

    Removed shrooms.lua
    Removed because I'm not planning to use it anymore

--- mods/foodstuff/shrooms.lua
+++
@@ -1,5 +0,0 @@
-minetest.register_craftitem("foodstuff:stewed_mushroom_red", {
-	description = "Stewed Red Mushroom",
-	inventory_image = "stewed_mushroom_red.png",
-	on_use = minetest.item_eat(3),
-})
