77236c6b1dcfff8fcfa29d33bc3d21bcbb819757
Eric Rustrum <rustrumek@gmail.com>
2021-08-17 10:21:02 -0700

    Modified ore generation

--- mods/rocks/ores.lua
+++ mods/rocks/ores.lua
@@ -14,15 +14,31 @@ minetest.register_node("rocks:granite_with_iron", {
 	legacy_mineral = true,
 })
 
+minetest.register_node("rocks:granite_with_copper", {
+	description = "Granite with Copper",
+	tiles = {"granite_with_copper.png"},
+	groups = {cracky = 3},
+	drop = "default:copper_lump",
+	legacy_mineral = true,
+})
+
+minetest.register_node("rocks:granite_with_tin", {
+	description = "Granite with Tin",
+	tiles = {"granite_with_tin.png"},
+	groups = {cracky = 3},
+	drop = "default:tin_lump",
+	legacy_mineral = true,
+})
+
 
 
 minetest.register_ore({
 		ore_type       = "scatter",
-		ore            = "rocks:granite_with_coal",
+		ore            = "rocks:granite_with_tin",
 		wherein        = "rocks:granite",
 		clust_scarcity = 32 * 32 * 32,
-		clust_num_ores = 8,
-		clust_size     = 32,
+		clust_num_ores = 16,
+		clust_size     = 8,
 		y_max          = 31000,
 		y_min          = 0,
 	})
@@ -31,7 +47,7 @@ minetest.register_ore({
 		ore_type       = "scatter",
 		ore            = "rocks:granite_with_iron",
 		wherein        = "rocks:granite",
-		clust_scarcity = 48 * 48 * 48,
+		clust_scarcity = 64 * 64 * 64,
 		clust_num_ores = 16,
 		clust_size     = 8,
 		y_max          = 31000,
