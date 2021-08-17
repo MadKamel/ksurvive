027f33ea7b54d4eba42f7dd3823ce7031589146e
Eric Rustrum <rustrumek@gmail.com>
2021-08-17 09:54:54 -0700

    Added ore generation for granite.
    I think I am in love

--- mods/rocks/ores.lua
+++ mods/rocks/ores.lua
@@ -13,3 +13,27 @@ minetest.register_node("rocks:granite_with_iron", {
 	drop = "default:iron_lump",
 	legacy_mineral = true,
 })
+
+
+
+minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "rocks:granite_with_coal",
+		wherein        = "rocks:granite",
+		clust_scarcity = 32 * 32 * 32,
+		clust_num_ores = 8,
+		clust_size     = 32,
+		y_max          = 31000,
+		y_min          = 0,
+	})
+
+minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "rocks:granite_with_iron",
+		wherein        = "rocks:granite",
+		clust_scarcity = 48 * 48 * 48,
+		clust_num_ores = 16,
+		clust_size     = 8,
+		y_max          = 31000,
+		y_min          = 0,
+	})
