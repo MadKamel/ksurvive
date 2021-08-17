ddb2cb77e505b61181c4de2ab4dac20bb8d6e8c7
Eric Rustrum <rustrumek@gmail.com>
2021-08-17 09:41:39 -0700

    Added stone axe heads
    Please help I am being held against my will by a girl named Phoebe I am scared

--- mods/rocks/toolheads.lua
+++ mods/rocks/toolheads.lua
@@ -39,3 +39,24 @@ minetest.register_craftitem("rocks:shovel_head_basalt", {
 	description = "Basalt Shovel Head",
 	inventory_image = "toolhead_basalt_shovel.png"
 })
+
+
+minetest.register_craftitem("rocks:axe_head_stone", {
+	description = "Stone Axe Head",
+	inventory_image = "toolhead_stone_axe.png"
+})
+
+minetest.register_craftitem("rocks:axe_head_desert_stone", {
+	description = "Desert Stone Axe Head",
+	inventory_image = "toolhead_desertstone_axe.png"
+})
+
+minetest.register_craftitem("rocks:axe_head_granite", {
+	description = "Granite Axe Head",
+	inventory_image = "toolhead_granite_axe.png"
+})
+
+minetest.register_craftitem("rocks:axe_head_basalt", {
+	description = "Basalt Axe Head",
+	inventory_image = "toolhead_basalt_axe.png"
+})
