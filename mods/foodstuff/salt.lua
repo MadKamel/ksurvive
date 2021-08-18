e81a5a821899d4a76421a85058607f478830886f
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 13:01:09 -0700

    Added salt to the salt groups that correspond.

--- mods/foodstuff/salt.lua
+++ mods/foodstuff/salt.lua
@@ -1,18 +1,20 @@
 minetest.register_craftitem("foodstuff:bitter_salt", {
 	description = "Bitter Salt",
 	inventory_image = "bittersalt.png",
+	groups = {salt = 1}
 })
 
 minetest.register_craftitem("foodstuff:salt", {
 	description = "Salt",
 	inventory_image = "salt.png",
+	groups = {salt = 1}
 })
 
 minetest.register_node("foodstuff:bitter_salt_block", {
 	description = "Bitter Salt Block",
 	tiles = {"bittersalt_block.png"},
 	paramtype = "light",
-	groups = {falling_node = 1, dig_immediate = 3, salt = 1},
+	groups = {falling_node = 1, dig_immediate = 3, salt_block = 1},
 	on_use = minetest.item_eat(3),
 })
 
@@ -20,6 +22,6 @@ minetest.register_node("foodstuff:salt_block", {
 	description = "Salt Block",
 	tiles = {"salt_block.png"},
 	paramtype = "light",
-	groups = {falling_node = 1, dig_immediate = 3, salt = 1},
+	groups = {falling_node = 1, dig_immediate = 3, salt_block = 1},
 	on_use = minetest.item_eat(2),
 })
