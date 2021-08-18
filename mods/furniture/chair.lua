55079056993ce9f5e7bc2f44124645313779c432
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 12:01:43 -0700

    Added iron chair, just as a test.

--- mods/furniture/chair.lua
+++ mods/furniture/chair.lua
@@ -33,3 +33,39 @@ minetest.register_node("furniture:chair", {
 	},
 	groups = {falling_node = 1, dig_immediate = 3},
 })
+
+minetest.register_node("furniture:iron_chair", {
+	description = "Wrought Iron Chair",
+	tiles = {"iron_block.png"},
+	drawtype = "nodebox",
+	sunlight_propagates = true,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3125, -0.5, 0.1875, -0.1875, 0.5, 0.3125},
+			{0.1875, -0.5, 0.1875, 0.3125, 0.5, 0.3125},
+			{0.1875, -0.5, -0.3125, 0.3125, -0.0625, -0.1875},
+			{-0.3125, -0.5, -0.3125, -0.1875, -0.0625, -0.1875},
+			{-0.3125, -0.125, -0.3125, 0.3125, 0, 0.3125},
+			{-0.1875, 0.3125, 0.1875, 0.1875, 0.4375, 0.3125},
+			{-0.3125, 0.125, 0.1875, 0.3125, 0.1875, 0.3125},
+			{0.23, -0.4375, -0.3125, 0.29, -0.375, 0.3125},
+			{-0.29, -0.4375, -0.3125, -0.23, -0.375, 0.3125},
+			{-0.29, -0.4375, -0.0315, 0.29, -0.375, 0.031},
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3125, -0.5, -0.3125, 0.3125, 0.5, 0.3125},
+	},
+	collision_box={
+		type="fixed",
+		fixed={
+			{-0.3125, -0.5, -0.3125, 0.3125, -0.0625, 0.3125},
+			{-0.3125, -0.5, 0.1875, -0.1875, 0.5, 0.3125},
+		},
+	},
+	groups = {falling_node = 1, cracky = 3},
+})
