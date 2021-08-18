77a7c4bb0764c677fe0ab52a4b4695c8b9fb7b5c
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 11:58:48 -0700

    Removed legacy item concrete knife

--- mods/concrete/knife.lua
+++
@@ -1,9 +0,0 @@
-minetest.register_tool("concrete:knife", {
-	description = "Concrete Knife",
-	inventory_image = "concrete_knife.png",
-	tool_capabilities = {
-		full_punch_interval = 2.5
-	},
-	sound = {breaks = "default_tool_breaks"},
-	groups = {concrete_knife = 1}
-})
