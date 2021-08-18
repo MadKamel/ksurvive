645d8ecf9f06d1e53b730592b38dcac508153015
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 13:04:48 -0700

    Made sugar edible

--- mods/foodstuff/sugar.lua
+++ mods/foodstuff/sugar.lua
@@ -1,7 +1,8 @@
 minetest.register_craftitem("foodstuff:apple_sugar", {
 	description = "Apple Sugar",
 	inventory_image = "apple_sugar.png",
-  groups = {sugar = 1}
+	groups = {sugar = 1},
+	on_use = minetest.item_eat(1),
 })
 
 minetest.register_node("foodstuff:apple_sugar_block", {
@@ -9,5 +10,4 @@ minetest.register_node("foodstuff:apple_sugar_block", {
 	tiles = {"apple_sugar_block.png"},
 	paramtype = "light",
 	groups = {falling_node = 1, dig_immediate = 3, sugar_block = 1},
-	on_use = minetest.item_eat(3),
 })
