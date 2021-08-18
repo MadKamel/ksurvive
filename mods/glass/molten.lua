614d52d798539fc31b852a7640b1c57db6de7dba
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 11:56:55 -0700

    Added glass re-smelting

--- mods/glass/molten.lua
+++ mods/glass/molten.lua
@@ -1,12 +1,3 @@
-bucket.register_liquid(
-	"glass:molten_glass_source",
-	"glass:molten_glass_flowing",
-	"glass:molten_glass_bucket",
-	"bucket_glass.png",
-	"Wooden Bucket with Molten Glass",
-	{tool = 1}
-)
-
 minetest.register_node("glass:molten_glass_source", {
 	description = "Glass Source Node",
 	drawtype = "liquid",
@@ -136,7 +127,7 @@ minetest.register_abm({
 
 minetest.register_abm({
 	neighbors = {"group:igniter"},
-	nodenames = {"default:sand"},
+	nodenames = {"group:sand"},
 	interval = 0.5,
 	chance = 5,
 	action = function(pos, node, active_object_count, active_object_count_wider)
@@ -147,18 +138,7 @@ minetest.register_abm({
 
 minetest.register_abm({
 	neighbors = {"group:igniter"},
-	nodenames = {"default:silver_sand"},
-	interval = 0.5,
-	chance = 15,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		local pos = {x = pos.x, y = pos.y, z = pos.z}
-		minetest.set_node(pos, {name = "glass:molten_glass_source"})
-	end
-})
-
-minetest.register_abm({
-	neighbors = {"group:igniter"},
-	nodenames = {"default:desert_sand"},
+	nodenames = {"group:glass"},
 	interval = 0.5,
 	chance = 15,
 	action = function(pos, node, active_object_count, active_object_count_wider)
