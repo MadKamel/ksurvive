a3a9deb434c81ad91a0509901d4be9390c320c0f
Eric Rustrum <rustrumek@gmail.com>
2021-08-19 14:24:31 -0700

    Added wooden spool craftitem

--- mods/nature/weaving.lua
+++ mods/nature/weaving.lua
@@ -3,3 +3,9 @@ minetest.register_craftitem("nature:juncus_fiber", {
 	inventory_image = "juncus_fiber.png",
 	groups = {thread = 1},
 })
+
+minetest.register_craftitem("nature:wood_spool", {
+	description = "Wooden Spool",
+	inventory_image = "wood_spool.png",
+	groups = {spool = 1},
+})
