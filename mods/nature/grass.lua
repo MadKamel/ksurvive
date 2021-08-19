00322bb2be09b0949e8dc6adfdbe412e55538c66
Eric Rustrum <rustrumek@gmail.com>
2021-08-19 12:04:17 -0700

    Added juncus rushes nodes
    I just need to rename the texture files to "juncus_x". Probably will do this later.

--- mods/nature/grass.lua
+++ mods/nature/grass.lua
@@ -118,3 +118,126 @@ minetest.register_node("nature:bahia", {
 		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
 	}
 })
+
+
+
+minetest.register_node("nature:juncus_0", {
+	description = "Juncus Seeds",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"juncus_0.png"},
+	inventory_image = "bahia_seeds.png",
+	wield_image = "bahia_seeds.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = f_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+	}
+})
+
+minetest.register_node("nature:juncus_1", {
+	description = "Juncus Sprouts",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"juncus_1.png"},
+	inventory_image = "juncus_1.png",
+	wield_image = "juncus_1.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = f_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+	}
+})
+
+minetest.register_node("nature:juncus_2", {
+	description = "Young Juncus Rushes",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"juncus_2.png"},
+	inventory_image = "juncus_2.png",
+	wield_image = "juncus_2.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = f_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+	}
+})
+
+minetest.register_node("nature:juncus_3", {
+	description = "Young Juncus Rushes",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"juncus_3.png"},
+	inventory_image = "juncus_3.png",
+	wield_image = "juncus_3.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = f_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+	}
+})
+
+minetest.register_node("nature:juncus_4", {
+	description = "Mature Juncus Grass",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"juncus_4.png"},
+	inventory_image = "juncus_4.png",
+	wield_image = "juncus_4.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = f_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+	},
+    drop = {
+		max_items = 2,
+		items = {
+			{items = {"nature:juncus_0 2"}},
+			{items = {"nature:juncus"}}
+		}
+	}
+})
+
+minetest.register_node("nature:juncus", {
+	description = "Juncus Rushes",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"juncus_4.png"},
+	inventory_image = "juncus_4.png",
+	wield_image = "juncus_4.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = f_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 3 / 16, 2 / 16},
+	}
+})
