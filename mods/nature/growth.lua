4339ac9fa8b71a97c0a6004554ab94c8c0a821d4
Eric Rustrum <rustrumek@gmail.com>
2021-08-19 10:29:29 -0700

    Added growth cycle for bahiagrass
    This is cool, can't wait to actually run the game now!

--- mods/nature/growth.lua
+++ mods/nature/growth.lua
@@ -1,5 +1,11 @@
+-- Default growth params
+
 local growthrate = 2
 local growchance = 25
+
+
+
+-- This is for the black tulip's growth cycle.
 
 minetest.register_abm({
     nodenames = {"nature:black_tulip_0"},
@@ -42,5 +48,53 @@ minetest.register_abm({
             active_object_count_wider)
         local pos = {x = pos.x, y = pos.y, z = pos.z}
         minetest.set_node(pos, {name = "nature:black_tulip_4"})
+    end
+})
+
+
+
+-- This is for the bahiagrass's growth cycle.
+
+minetest.register_abm({
+    nodenames = {"nature:bahia_0"},
+    interval = growthrate,
+    chance = growchance,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local pos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(pos, {name = "nature:bahia_1"})
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"nature:bahia_1"},
+    interval = growthrate,
+    chance = growchance,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local pos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(pos, {name = "nature:bahia_2"})
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"nature:bahia_2"},
+    interval = growthrate,
+    chance = growchance,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local pos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(pos, {name = "nature:bahia_3"})
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"nature:bahia_3"},
+    interval = growthrate,
+    chance = growchance,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local pos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(pos, {name = "nature:bahia_4"})
     end
 })
