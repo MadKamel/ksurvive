9fea10367d9585b8f51efbea4e75b84f4890f010
Eric Rustrum <rustrumek@gmail.com>
2021-08-19 14:32:01 -0700

    Make applesauce a replacing food item.

--- mods/foodstuff/applesauce.lua
+++ mods/foodstuff/applesauce.lua
@@ -1,5 +1,5 @@
 minetest.register_craftitem("foodstuff:applesauce", {
 	description = "Applesauce",
 	inventory_image = "applesauce.png",
-	on_use = minetest.item_eat(10),
+	on_use = minetest.item_eat(10, "bucket:bucket_empty"),,
 })
