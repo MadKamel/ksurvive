c8537d998505ae423d03797f9ba6f1afaac342b5
Eric Rustrum <rustrumek@gmail.com>
2021-08-20 13:25:40 -0700

    Added bouteloua plant nodes.

--- mods/nature/grass.lua
+++ mods/nature/grass.lua
@@ -215,3 +215,113 @@ minetest.register_node("nature:juncus_4", {
 		}
 	},
 })
+
+
+
+minetest.register_node("nature:bouteloua_0", {
+	description = "Bouteloua Seeds",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"bouteloua_0.png"},
+	inventory_image = "bouteloua_seeds.png",
+	wield_image = "bouteloua_seeds.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = g_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-6 / 16, -0.5, -6 / 16, 6 / 16, -5 / 16, 6 / 16},
+	},
+	drop = {}
+})
+
+minetest.register_node("nature:bouteloua_1", {
+	description = "Bouteloua Sprouts",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"bouteloua_1.png"},
+	inventory_image = "bouteloua_1.png",
+	wield_image = "bouteloua_1.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = g_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-6 / 16, -0.5, -6 / 16, 6 / 16, -5 / 16, 6 / 16},
+	},
+	drop = {}
+})
+
+minetest.register_node("nature:bouteloua_2", {
+	description = "Young Bouteloua",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"bouteloua_2.png"},
+	inventory_image = "bouteloua_2.png",
+	wield_image = "bouteloua_2.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = g_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-6 / 16, -0.5, -6 / 16, 6 / 16, -3 / 16, 6 / 16},
+	},
+	drop = {}
+})
+
+minetest.register_node("nature:bouteloua_3", {
+	description = "Young Bouteloua",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"bouteloua_3.png"},
+	inventory_image = "bouteloua_3.png",
+	wield_image = "bouteloua_3.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = g_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-6 / 16, -0.5, -6 / 16, 6 / 16, -3 / 16, 6 / 16},
+	},
+	drop = {}
+})
+
+minetest.register_node("nature:bouteloua_3", {
+	description = "Mature Bouteloua",
+	drawtype = "plantlike",
+	waving = 1,
+	tiles = {"bouteloua_4.png"},
+	inventory_image = "bouteloua_4.png",
+	wield_image = "bouteloua_4.png",
+	sunlight_propagates = true,
+	paramtype = "light",
+	walkable = false,
+	buildable_to = true,
+	groups = g_groups,
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-6 / 16, -0.5, -6 / 16, 6 / 16, -3 / 16, 6 / 16},
+	},
+	drop = {
+		max_items = 3,
+		items = {
+			{items = {"nature:bouteloua_0 6"}},
+			{items = {"nature:bouteloua_0 1"}, rarity = 2},
+			{items = {"nature:bouteloua_0 1"}, rarity = 4},
+			{items = {"nature:bouteloua_0 1"}, rarity = 6},
+		}
+	},
+})
