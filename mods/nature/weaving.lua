f85c1c5017035ca6671676b857d63357f2fe18a6
Eric Rustrum <rustrumek@gmail.com>
2021-08-20 09:47:31 -0700

    Added juncus block

--- mods/nature/weaving.lua
+++ mods/nature/weaving.lua
@@ -7,3 +7,10 @@ minetest.register_craftitem("nature:juncus_patch", {
 	description = "Patch of Woven Juncus Stalk",
 	inventory_image = "juncus_patch.png",
 })
+
+minetest.register_node("nature:juncus_block", {
+	tiles = {"juncus_block.png"},
+	description = "Block of Woven Juncus Stalk",
+	groups = {snappy = 3, falling_node = 1},
+	walkable = true,
+})
