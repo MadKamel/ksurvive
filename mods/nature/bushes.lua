f3d2f9fc2775d34e1c5fa5c8c899bdf2d2e3b078
Eric Rustrum <rustrumek@gmail.com>
2021-08-21 11:21:43 -0700

    Added gin berries texture

--- mods/nature/bushes.lua
+++ mods/nature/bushes.lua
@@ -1,6 +1,6 @@
 minetest.register_craftitem("nature:gin_berries", {
 	description = "Gin Berries",
-	inventory_image = "default_blueberries.png",
+	inventory_image = "gin_berries.png",
 	groups = {food_berry = 1},
 	on_use = minetest.item_eat(3),
 })
