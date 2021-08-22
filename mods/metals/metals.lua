3cf3ddad08380cfec92ff92418698a5fd13cebd1
Eric Rustrum <rustrumek@gmail.com>
2021-08-21 20:35:39 -0700

    Added oxidized metals.

--- mods/metals/metals.lua
+++ mods/metals/metals.lua
@@ -1,7 +1,7 @@
 minetest.register_node("metals:iron_block", {
 	description = "Block of Refined Iron",
 	tiles = {"iron_block.png"},
-	groups = {cracky = 2},
+	groups = {cracky = 2, iron_block = 1},
 })
 
 minetest.register_node("metals:glowing_iron_block", {
@@ -10,10 +10,16 @@ minetest.register_node("metals:glowing_iron_block", {
 	groups = {glowing_metal = 3},
 })
 
+minetest.register_node("metals:rusted_iron_block", {
+	description = "Block of Refined Iron with Oxidization",
+	tiles = {"iron_block.png^iron_rust.png"},
+	groups = {cracky = 2, iron_block = 1},
+})
+
 minetest.register_node("metals:copper_block", {
 	description = "Block of Refined Copper",
 	tiles = {"copper_block.png"},
-	groups = {cracky = 2},
+	groups = {cracky = 2, copper_block = 1},
 })
 
 minetest.register_node("metals:glowing_copper_block", {
@@ -22,10 +28,16 @@ minetest.register_node("metals:glowing_copper_block", {
 	groups = {cracky = 2},
 })
 
+minetest.register_node("metals:rusted_copper_block", {
+	description = "Block of Refined Copper with OXidization",
+	tiles = {"copper_block.png^copper_rust.png"},
+	groups = {cracky = 2, copper_block = 1},
+})
+
 minetest.register_node("metals:tin_block", {
 	description = "Block of Refined Tin",
 	tiles = {"tin_block.png"},
-	groups = {glowing_metal = 3},
+	groups = {cracky = 2, tin_block = 1},
 })
 
 minetest.register_node("metals:glowing_tin_block", {
@@ -37,7 +49,7 @@ minetest.register_node("metals:glowing_tin_block", {
 minetest.register_node("metals:gold_block", {
 	description = "Block of Refined Gold",
 	tiles = {"gold_block.png"},
-	groups = {cracky = 2},
+	groups = {cracky = 2, gold_block = 1},
 })
 
 minetest.register_node("metals:glowing_gold_block", {
@@ -49,7 +61,7 @@ minetest.register_node("metals:glowing_gold_block", {
 minetest.register_node("metals:bronze_block", {
 	description = "Block of Bronze",
 	tiles = {"bronze_block.png"},
-	groups = {cracky = 2},
+	groups = {cracky = 2, bronze_block = 1},
 })
 
 minetest.register_node("metals:glowing_bronze_block", {
