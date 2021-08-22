058d6331b439c131302c7ad47c66d238347ee2ab
Eric Rustrum <rustrumek@gmail.com>
2021-08-21 20:52:04 -0700

    Added more ores.

--- mods/rocks/ores.lua
+++ mods/rocks/ores.lua
@@ -1,6 +1,6 @@
 minetest.register_node("rocks:granite_with_coal", {
 	description = "Granite with Coal",
-	tiles = {"granite_with_coal.png"},
+	tiles = {"granite_stone.png^default_mineral_coal"},
 	groups = {cracky = 3},
 	drop = "default:coal_lump",
 	legacy_mineral = true,
@@ -8,7 +8,7 @@ minetest.register_node("rocks:granite_with_coal", {
 
 minetest.register_node("rocks:granite_with_iron", {
 	description = "Granite with Iron",
-	tiles = {"granite_with_iron.png"},
+	tiles = {"granite_stone.png^default_mineral_iron"},
 	groups = {cracky = 3},
 	drop = "default:iron_lump",
 	legacy_mineral = true,
@@ -16,7 +16,7 @@ minetest.register_node("rocks:granite_with_iron", {
 
 minetest.register_node("rocks:granite_with_copper", {
 	description = "Granite with Copper",
-	tiles = {"granite_with_copper.png"},
+	tiles = {"granite_stone.png^default_mineral_copper"},
 	groups = {cracky = 3},
 	drop = "default:copper_lump",
 	legacy_mineral = true,
@@ -24,9 +24,17 @@ minetest.register_node("rocks:granite_with_copper", {
 
 minetest.register_node("rocks:granite_with_tin", {
 	description = "Granite with Tin",
-	tiles = {"granite_with_tin.png"},
+	tiles = {"granite_stone.png^default_mineral_tin"},
 	groups = {cracky = 3},
 	drop = "default:tin_lump",
+	legacy_mineral = true,
+})
+
+minetest.register_node("rocks:granite_with_gold", {
+	description = "Granite with Gold",
+	tiles = {"granite_stone.png^default_mineral_gold"},
+	groups = {cracky = 3},
+	drop = "default:gold_lump",
 	legacy_mineral = true,
 })
 
@@ -35,5 +43,37 @@ minetest.register_node("rocks:basalt_with_coal", {
 	tiles = {"basalt_stone.png^default_mineral_coal.png"},
 	groups = {cracky = 3},
 	drop = "default:coal_lump",
+	legacy_mineral = true,
+})
+
+minetest.register_node("rocks:basalt_with_iron", {
+	description = "Basalt with Iron",
+	tiles = {"basalt_stone.png^default_mineral_iron.png"},
+	groups = {cracky = 3},
+	drop = "default:iron_lump",
+	legacy_mineral = true,
+})
+
+minetest.register_node("rocks:basalt_with_copper", {
+	description = "Basalt with Copper",
+	tiles = {"basalt_stone.png^default_mineral_copper.png"},
+	groups = {cracky = 3},
+	drop = "default:copper_lump",
+	legacy_mineral = true,
+})
+
+minetest.register_node("rocks:basalt_with_tin", {
+	description = "Basalt with Tin",
+	tiles = {"basalt_stone.png^default_mineral_tin.png"},
+	groups = {cracky = 3},
+	drop = "default:tin_lump",
+	legacy_mineral = true,
+})
+
+minetest.register_node("rocks:basalt_with_gold", {
+	description = "Basalt with Gold",
+	tiles = {"basalt_stone.png^default_mineral_gold.png"},
+	groups = {cracky = 3},
+	drop = "default:iron_lump",
 	legacy_mineral = true,
 })
