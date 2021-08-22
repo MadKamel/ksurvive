575718cd11216130f8d014f61c56fe9478ed6dbc
Eric Rustrum <rustrumek@gmail.com>
2021-08-21 20:15:28 -0700

    Added gold and bronze nodes.

--- mods/metals/metals.lua
+++ mods/metals/metals.lua
@@ -33,3 +33,27 @@ minetest.register_node("metals:glowing_tin_block", {
 	tiles = {"glowing_tin.png"},
 	groups = {glowing_metal = 3},
 })
+
+minetest.register_node("metals:gold_block", {
+	description = "Block of Refined Gold",
+	tiles = {"gold_block.png"},
+	groups = {cracky = 2},
+})
+
+minetest.register_node("metals:glowing_gold_block", {
+	description = "Block of Hot Gold",
+	tiles = {"glowing_gold.png"},
+	groups = {glowing_metal = 3},
+})
+
+minetest.register_node("metals:bronze_block", {
+	description = "Block of Bronze",
+	tiles = {"bronze_block.png"},
+	groups = {cracky = 2},
+})
+
+minetest.register_node("metals:glowing_bronze_block", {
+	description = "Block of Hot Bronze",
+	tiles = {"glowing_bronze.png"},
+	groups = {glowing_metal = 3},
+})
