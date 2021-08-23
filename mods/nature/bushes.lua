f49f5f2ad3ef1036f67174bf75c7c31809ec06de
Eric Rustrum <rustrumek@gmail.com>
2021-08-23 08:13:18 -0700

    Removed bushes.lua, not gonna need it anymore.

--- mods/nature/bushes.lua
+++
@@ -1,6 +0,0 @@
-minetest.register_craftitem("nature:gin_berries", {
-	description = "Gin Berries",
-	inventory_image = "gin_berries.png",
-	groups = {food_berry = 1},
-	on_use = minetest.item_eat(1),
-})
