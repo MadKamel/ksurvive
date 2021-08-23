9912719e0c326064782bfa55c0d3ca85a8137771
Eric Rustrum <rustrumek@gmail.com>
2021-08-23 14:27:58 -0700

    Added mortar and pestle.
    This could or could not be interesting

---
+++ mods/foodstuff/tools.lua
@@ -0,0 +1,11 @@
+minetest.register_craftitem("foodstuff:marble_mortar", {
+	description = "Marble Mortar",
+	inventory_image = "marble_mortar.png",
+	groups = {mortar = 1}
+})
+
+minetest.register_craftitem("foodstuff:marble_pestle", {
+	description = "Marble Pestle",
+	inventory_image = "marble_pestle.png",
+	groups = {pestle = 1}
+})
