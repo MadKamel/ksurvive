3c2e9bc29c34a512369216a2de9d6d14dff3317a
Eric Rustrum <rustrumek@gmail.com>
2021-08-23 11:04:48 -0700

    Added an apple

--- mods/foodstuff/apple.lua
+++ mods/foodstuff/apple.lua
@@ -1,1 +1,6 @@
 -- TODO: add apple craftitems that replace with each stage of eating.
+minetest.register_craftitem("foodstuff:apple", {
+	description = "Apple",
+	inventory_image = "apple.png",
+	on_use = minetest.item_eat(1, "foodstuff:apple_eaten_0"),
+})
