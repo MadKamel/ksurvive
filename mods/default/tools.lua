725146e4680e5524c2870aecdacdf56cea8231e8
Eric Rustrum <rustrumek@gmail.com>
2021-08-25 09:33:49 -0700

    Disallowed fists from digging crumbly=2 nodes.

--- mods/default/tools.lua
+++ mods/default/tools.lua
@@ -12,7 +12,7 @@ minetest.override_item("", {
 		full_punch_interval = 0.9,
 		max_drop_level = 0,
 		groupcaps = {
-			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			crumbly = {times={[3]=0.70}, uses=0, maxlevel=1},
 			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
 			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
 		},
