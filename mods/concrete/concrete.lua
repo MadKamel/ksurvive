00ddcb6a0b89c6db7cc372bd067fbaddeaefb623
Eric Rustrum <rustrumek@gmail.com>
2021-09-27 08:43:11 -0700

    Changed concrete texture.
    This was long due to happen...

--- mods/concrete/concrete.lua
+++ mods/concrete/concrete.lua
@@ -1,6 +1,6 @@
 minetest.register_node("concrete:concrete", {
 	description = "Concrete",
-	tiles = {"concrete_block.png"},
+	tiles = {"concrete_node.png"},
 	paramtype = "light",
 	groups = {cracky = 2},
 })
